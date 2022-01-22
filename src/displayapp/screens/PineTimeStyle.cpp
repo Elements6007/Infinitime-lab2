431043e84acff339f6801b36fdb5d8cd0eaf0372
Elements6007 <elements6007@gmail.com>
2022-01-22 14:08:20 -0500

    push

--- src/displayapp/screens/PineTimeStyle.cpp
+++ src/displayapp/screens/PineTimeStyle.cpp
@@ -340,6 +340,11 @@ bool PineTimeStyle::OnButtonPushed() {
 void PineTimeStyle::SetBatteryIcon() {
   auto batteryPercent = batteryPercentRemaining.Get();
   lv_label_set_text(batteryIcon, BatteryIcon::GetBatteryIcon(batteryPercent));
+   if (batteryPercent == 100) {
+      lv_obj_set_style_local_text_color(batteryIcon, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_GREEN);
+    } else {
+      lv_obj_set_style_local_text_color(batteryIcon, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    }
 }
 
 void PineTimeStyle::AlignIcons() {
