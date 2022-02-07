0427696f2367a2798bfcea8bbedf0bcd404d1f15
Elements6007 <elements6007@gmail.com>
2022-02-07 15:30:39 -0500

    test 26

--- src/displayapp/screens/settings/SettingSetDate.cpp
+++ src/displayapp/screens/settings/SettingSetDate.cpp
@@ -204,7 +204,7 @@ std::unique_ptr<Screen> SettingSetDate::CreateScreen1() {
      lblMonth, Pinetime::Controllers::DateTime::MonthShortToStringLow(static_cast<Pinetime::Controllers::DateTime::Months>(monthValue)));
  }
 
-  return std::make_unique<Screens::Label>(0, 2, app, dateTimeController);
+  return std::make_unique<Screens::Label>(0, 2, app, title);
   // title, icon, lblDay, lblMonth, lblYear, btnDayPlus, btnDayMinus, btnMonthPlus, btnMonthMinus, btnYearPlus, btnYearMinus, btnSetTime
 }
 
