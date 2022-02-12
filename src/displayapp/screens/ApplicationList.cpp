99b5ca66535c06ce3627938846943f286ed23126
Elements6007 <elements6007@gmail.com>
2022-02-11 22:03:38 -0500

    test 42 fix 2

--- src/displayapp/screens/ApplicationList.cpp
+++ src/displayapp/screens/ApplicationList.cpp
@@ -66,7 +66,7 @@ std::unique_ptr<Screen> ApplicationList::CreateScreen2() {
 
 std::unique_ptr<Screen> ApplicationList::CreateScreen3() {
   std::array<Screens::Tile::Applications, 6> applications {
-          {{"p", Apps::Pagetest},
+          {{"p", Apps::pagetest},
            {"", Apps::None},
            {"", Apps::None},
            {"", Apps::None},
