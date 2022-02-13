f9ff3105703de77e8a06d818578286524b197a38
Elements6007 <elements6007@gmail.com>
2022-02-12 19:24:54 -0500

    test 50

--- src/displayapp/screens/pagetest.cpp
+++ src/displayapp/screens/pagetest.cpp
@@ -1,4 +1,24 @@
 #include "displayapp/screens/pagetest.h"
+#include "displayapp/DisplayApp.h"
+
+using namespace Pinetime::Applications::Screens;
+
+pagetest::pagetest(DisplayApp* app) : Screen(app) {
+  lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
+  lv_label_set_text_static(title, "My test application");
+  lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
+  lv_obj_align(title, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
+}
+
+pagetest::~pagetest() {
+  lv_obj_clean(lv_scr_act());
+}
+
+
+
+
+
+/*#include "displayapp/screens/pagetest.h"
 #include "displayapp/screens/Screen.h"
 #include "displayapp/DisplayApp.h"
 #include "displayapp/screens/Label.h"
@@ -43,4 +63,4 @@ pagetest::~pagetest() {
 bool pagetest::OnTouchEvent(Pinetime::Applications::TouchEvents event) {
   return screens.OnTouchEvent(event);
 }
-
+*/
