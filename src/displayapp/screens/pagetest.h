c25e0aed66e72d8e835c65f94204618c3e03b63c
Elements6007 <elements6007@gmail.com>
2022-02-12 22:23:43 -0500

    test 51

--- src/displayapp/screens/pagetest.h
+++ src/displayapp/screens/pagetest.h
@@ -1,32 +1,16 @@
 #pragma once
 
-#include <memory>
-#include <lvgl/lvgl.h>
 #include "displayapp/screens/Screen.h"
-#include "displayapp/screens/ScreenList.h"
+#include <lvgl/lvgl.h>
 
 namespace Pinetime {
-
   namespace Applications {
-    class DisplayApp;
-
     namespace Screens {
-      class pagetest : public Screen {
+      class MyApp : public Screen {
       public:
-        explicit pagetest(DisplayApp* app);
+        pagetest(DisplayApp* app);
         ~pagetest() override;
-        bool OnTouchEvent(TouchEvents event) override;
-
-
-       // private:
-        
-        //lv_obj_t* onion;
-        //lv_obj_t* obama;
-
-      //  ScreenList<2> screens;
-
-      //  std::unique_ptr<Screen> CreateScreen1();
-      //  std::unique_ptr<Screen> CreateScreen2();
       };
     }
   }
+}
