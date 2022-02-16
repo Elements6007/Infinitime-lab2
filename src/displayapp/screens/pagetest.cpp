0e9cd526bef9884fa861d0a3562edb84a544322f
Elements6007 <elements6007@gmail.com>
2022-02-16 16:25:29 -0500

    test 62

--- src/displayapp/screens/pagetest.cpp
+++ src/displayapp/screens/pagetest.cpp
@@ -5,7 +5,7 @@ using namespace Pinetime::Applications::Screens;
 
 //taskRefresh = lv_task_create(RefreshTaskCallback, LV_DISP_DEF_REFR_PERIOD, LV_TASK_PRIO_MID, this);
 
-pagetest::pagetest(DisplayApp* app) : Screen(app) {
+pagetest::pagetest(Pinetime::Applications::DisplayApp *app) : Screen(app) {
   lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text_static(title, "My test application");
   lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
