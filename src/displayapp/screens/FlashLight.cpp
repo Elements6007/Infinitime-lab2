675aae7ea90da4abbfb1c926565f7dba3122cf3e
Elements6007 <elements6007@gmail.com>
2022-03-26 19:20:36 -0400

    p u s h

--- src/displayapp/screens/FlashLight.cpp
+++ src/displayapp/screens/FlashLight.cpp
@@ -24,7 +24,7 @@ FlashLight::FlashLight(Pinetime::Applications::DisplayApp* app,
 {
   brightnessController.Backup();
 
- brightnessLevel = FlashLight.Level();
+  brightnessLevel = Flashlight;
 
   flashLight = lv_label_create(lv_scr_act(), nullptr);
   lv_obj_set_style_local_text_font(flashLight, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &lv_font_sys_48);
@@ -151,7 +151,7 @@ bool FlashLight::OnTouchEvent(Pinetime::Applications::TouchEvents event) {
   return false;
 }
 
-void FlashLight::UpdateSelected(lv_obj_t* object, lv_event_t event) {
+/*void FlashLight::UpdateSelected(lv_obj_t* object, lv_event_t event) {
   if (event == LV_EVENT_VALUE_CHANGED) {
     for (uint8_t i = 0; i < optionsTotal; i++) {
       if (object == cbOption[i]) {
@@ -170,6 +170,6 @@ void FlashLight::UpdateSelected(lv_obj_t* object, lv_event_t event) {
       }
     }
   }
-}
+}*/
 
 
