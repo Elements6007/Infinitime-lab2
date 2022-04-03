c3fa11b85984688292df66c7ceff75c40e2ca402
Elements6007 <elements6007@gmail.com>
2022-04-03 14:10:27 -0400

    rm src

--- src/buttonhandler/ButtonActions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace Pinetime {
-  namespace Controllers {
-    enum class ButtonActions { None, Click, DoubleClick, LongPress, LongerPress };
-  }
-}
--- src/displayapp/TouchEvents.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace Pinetime {
-  namespace Applications {
-
-    enum class TouchEvents { None, Tap, SwipeLeft, SwipeRight, SwipeUp, SwipeDown, LongTap, DoubleTap };
-  }
-}
