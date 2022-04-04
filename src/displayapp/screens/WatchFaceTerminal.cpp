b6232834efc7192df8d2ef0db4b40a2cee6fb9b2
Elements6007 <elements6007@gmail.com>
2022-04-03 20:56:58 -0400

    Update WatchFaceTerminal.cpp

--- src/displayapp/screens/WatchFaceTerminal.cpp
+++ src/displayapp/screens/WatchFaceTerminal.cpp
@@ -84,9 +84,8 @@ WatchFaceTerminal::~WatchFaceTerminal() {
 }
 
 int loop() {
-  lv_obj_set_hidden(label_prompt_2, true);
-  sleep(5000);
-  !lv_obj_set_hidden(label_prompt_2, true);
+  lv_obj_set_hidden(lv_obj_t* label_prompt_2, true);
+  lv_obj_set_hidden(lv_obj_t* label_prompt_2, false);
 } 
 
 void WatchFaceTerminal::Refresh() {
