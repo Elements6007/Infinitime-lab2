6d97bdc672a29aca2081f6d0020052f28d1b65be
Elements6007 <elements6007@gmail.com>
2022-04-14 16:00:53 -0400

    Update FlashLight.h

--- src/displayapp/screens/FlashLight.h
+++ src/displayapp/screens/FlashLight.h
@@ -3,6 +3,7 @@
 #include "displayapp/screens/Screen.h"
 #include "components/brightness/BrightnessController.h"
 #include "systemtask/SystemTask.h"
+#include "components/settings/Settings.h"
 #include <cstdint>
 #include <lvgl/lvgl.h>
 
@@ -13,18 +14,35 @@ namespace Pinetime {
 
       class FlashLight : public Screen {
       public:
-        FlashLight(DisplayApp* app, System::SystemTask& systemTask, Controllers::BrightnessController& brightness);
+        FlashLight(DisplayApp* app, System::SystemTask& systemTask, Controllers::BrightnessController& brightness, Controllers::Settings& settingsController);
         ~FlashLight() override;
 
         bool OnTouchEvent(Pinetime::Applications::TouchEvents event) override;
         void OnClickEvent(lv_obj_t* obj, lv_event_t event);
 
+        enum class Levels { Off, Low, Medium, High };
+        void Init();
+
+        void Set(Levels level);
+        Levels Level() const;
+        void BrightnessSave();
+
+        void Backup();
+        void Restore();
+
       private:
+        
+
         void SetIndicators();
         void SetColors();
+       
+
+        Levels level = Levels::High;
+        Levels backupLevel = Levels::High;
 
         Pinetime::System::SystemTask& systemTask;
         Controllers::BrightnessController& brightnessController;
+        Controllers::Settings& settingsController;
 
         Controllers::BrightnessController::Levels brightnessLevel;
 
