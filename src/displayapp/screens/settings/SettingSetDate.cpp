538cab40c14e2400214e094a4829169a4a96ed4d
Elements6007 <elements6007@gmail.com>
2022-04-20 23:01:09 -0400

    Update SettingSetDate.cpp

--- src/displayapp/screens/settings/SettingSetDate.cpp
+++ src/displayapp/screens/settings/SettingSetDate.cpp
@@ -219,7 +219,7 @@ void SettingSetDate::CheckDay() {
 void SettingSetDate::UpdateMonthLabel() {
  lv_label_set_text_fmt(
     lblMonth, Pinetime::Controllers::DateTime::MonthShortToStringLow(static_cast<Pinetime::Controllers::DateTime::Months>(monthValue)));
-    lv_obj_align(lblMonth, lv_scr_act(), LV_ALIGN_CENTER, POS_X_DAY, POS_Y_TEXT);
+   /* lv_obj_align(lblMonth, lv_scr_act(), LV_ALIGN_CENTER, POS_X_DAY, POS_Y_TEXT);*/
 }
 
 std::unique_ptr<Screen> SettingSetDate::CreateScreen2() {
