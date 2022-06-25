9c9973df06cf87406fcf7f889206fef728730f6a
Elements6007 <elements6007@gmail.com>
2022-06-24 22:03:11 -0400

    push

--- src/displayapp/screens/settings/SettingTimeFormat.h
+++ src/displayapp/screens/settings/SettingTimeFormat.h
@@ -1,39 +1,28 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <lvgl/lvgl.h>
-#include "components/datetime/DateTimeController.h"
+
 #include "components/settings/Settings.h"
 #include "displayapp/screens/Screen.h"
 
 namespace Pinetime {
+
   namespace Applications {
     namespace Screens {
-      class SettingSetTime : public Screen {
-      public:
-        SettingSetTime(DisplayApp* app,
-                       Pinetime::Controllers::DateTime& dateTimeController,
-                       Pinetime::Controllers::Settings& settingsController);
-        ~SettingSetTime() override;
 
-        void HandleButtonPress(lv_obj_t* object, lv_event_t event);
+      class SettingTimeFormat : public Screen {
+      public:
+        SettingTimeFormat(DisplayApp* app, Pinetime::Controllers::Settings& settingsController);
+        ~SettingTimeFormat() override;
+
+        void UpdateSelected(lv_obj_t* object, lv_event_t event);
 
       private:
-        Controllers::DateTime& dateTimeController;
+        static constexpr std::array<const char*, 2> options = {" 12-hour", " 24-hour"};
         Controllers::Settings& settingsController;
-
-        void SetHourLabels();
-
-        int hoursValue;
-        int minutesValue;
-        lv_obj_t* lblHours;
-        lv_obj_t* lblMinutes;
-        lv_obj_t* lblampm;
-        lv_obj_t* btnHoursPlus;
-        lv_obj_t* btnHoursMinus;
-        lv_obj_t* btnMinutesPlus;
-        lv_obj_t* btnMinutesMinus;
-        lv_obj_t* btnSetTime;
+        lv_obj_t* cbOption[options.size()];
       };
     }
   }
